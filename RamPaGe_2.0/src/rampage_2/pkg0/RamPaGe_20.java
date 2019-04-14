f5b6fb731cdd1cf1168baa5d7b1424e460d8d5e9
Marito-R-T <mariomoises20008@gmail.com>
2019-04-14 16:25:43 -0600

    Proyecto Netbeans

--- RamPaGe_2.0/src/rampage_2/pkg0/RamPaGe_20.java
+++ RamPaGe_2.0/src/rampage_2/pkg0/RamPaGe_20.java
@@ -3,12 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.rampage_2;
+package rampage_2.pkg0;
 
 /**
  *
  * @author marito
  */
-public class Tanque {
+public class RamPaGe_20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
