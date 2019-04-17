3a4af4bea895a32953d7be3b2b17803804180230
Marito-R-T <mariomoises20008@gmail.com>
2019-04-17 14:48:53 -0600

    Netbeans

---
+++ RamPaGe_2.0/src/rampage_2/Juego/Mapas/Mapa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rampage_2.Juego.Mapas;
+
+import javax.swing.JPanel;
+import rampage_2.Juego.Mapas.Terrenos.Terreno;
+
+/**
+ *
+ * @author marito
+ */
+public class Mapa extends JPanel{
+    protected Terreno[][] mapa;
+}
---
+++ RamPaGe_2.0/src/rampage_2/Juego/Mapas/Mapa4x4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rampage_2.Juego.Mapas;
+
+import rampage_2.Juego.Mapas.Terrenos.Terreno;
+
+/**
+ *
+ * @author marito
+ */
+public class Mapa4x4 extends Mapa {
+
+    public Mapa4x4() {
+        super.mapa = new Terreno[4][4];
+    }
+}
